84d9d1e5394adf6796859c3690b59e39cc543470
ninjadev101 <sanchzxel@gmail.com>
2022-05-30 22:42:15 -0400

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+### description
+
+My Portfolio built with React.
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+Get the code.
+
+ <pre>git clone https://github.com/ninjadev101/react-portfolio.git</pre>
+
+Install required dependencies
+
+<pre>yarn install</pre>
+
+modify pages content in `src/content_option.js`.
+
+start the server
+
+<pre>yarn start</pre>
+
+### Thanks
+
+If you like this portfolio template don't forget give it a ⭐
---
+++ src/content_option.js
@@ -0,0 +1,190 @@
+const logotext = 'ERIC B.';
+const meta = {
+  title: 'Eric B.',
+  description:
+    'I’m Eric, a senior full stack devloper from Canada. I’m passionate about web, mobile and blockchain development and I love to create.'
+};
+
+const introdata = {
+  title: 'I’m Eric',
+  animated: {
+    first: 'I love coding',
+    second: 'I code cool websites',
+    third: 'I develop mobile apps'
+  },
+  description:
+    'A senior full stack devloper from Canada. I’m passionate about web, mobile and blockchain development and I love to contribute to startup companies.',
+  your_img_url: '/images/giphy.gif'
+};
+
+const dataabout = {
+  title: 'abit about my self',
+  aboutme:
+    'I’m Eric, a senior full stack devloper from Canada. \nMy goal is to create a better world through technology. I’m passionate about learning new technologies and building things that help people. \nMy previous roles have included a web developer, a mobile developer, a blockchain developer, a lead developer, a project manager, a cheif technical officer and a co-founder. \nI’m currently looking for a remote role in a startup company.'
+};
+
+const skills = [
+  {
+    name: 'Javascript',
+    value: 100
+  },
+  {
+    name: 'Typescript',
+    value: 85
+  },
+  {
+    name: 'Solidity',
+    value: 90
+  },
+  {
+    name: 'Jquery',
+    value: 100
+  },
+  {
+    name: 'ReactJS',
+    value: 90
+  },
+  {
+    name: 'VueJS',
+    value: 80
+  },
+  {
+    name: 'React Native',
+    value: 85
+  },
+  {
+    name: 'Flutter',
+    value: 75
+  },
+  {
+    name: 'NodeJS',
+    value: 90
+  },
+  {
+    name: 'Python',
+    value: 80
+  },
+  {
+    name: 'Rust',
+    value: 70
+  },
+  {
+    name: 'Golang',
+    value: 70
+  },
+  {
+    name: 'Others...',
+    value: 70
+  }
+];
+
+const services = [
+  {
+    title: 'Blockchain',
+    description:
+      'I am passionate about blockchain development. From smart contract development to metaverse development, I’m always looking for new ways to improve the web3.'
+  },
+  {
+    title: 'Web Development',
+    description:
+      'I am a huge fan of web development. I love to create beautiful and functional websites.'
+  },
+  {
+    title: 'Mobile Apps',
+    description:
+      'Making hybrid apps is my passion. I love to create apps for both iOS and Android using React Native or Flutter.'
+  }
+];
+
+const dataportfolio = [
+  {
+    img: 'https://smileconnect.ca/images/SmileConnect%20Logo.png',
+    desctiption: 'Find the right Invisalign provider near you today',
+    link: 'https://smileconnect.ca/'
+  },
+  {
+    img: 'https://assets-global.website-files.com/6193f428fc27ea2e5731ce0f/6193f428fc27ea3afc31cea8_logo.svg',
+    desctiption: 'Sponsorship decisions, simplified.',
+    link: 'https://www.sponsorpulseimi.com/'
+  },
+  {
+    img: 'https://waypointbuilding.com/images/light-logo.png',
+    desctiption:
+      'Data aggregation, dashboards, and workflows for Real Estate Teams.',
+    link: 'https://waypointbuilding.com/'
+  },
+  {
+    img: 'https://hashtagfox.com/_nuxt/img/logo.fde9247.svg',
+    desctiption: 'Find the best Hashtags for your Instagram Profile',
+    link: 'https://hashtagfox.com/'
+  },
+  {
+    img: 'https://standardsplus.pmi.org/images/pmi_logo_v5.png',
+    desctiption: 'PMIstandards+ Success at Your Fingertips',
+    link: 'https://standardsplus.pmi.org/'
+  },
+  {
+    img: 'https://static.wixstatic.com/media/120887_daa0f4a3728140a58693d643690c5ab8~mv2.png/v1/fill/w_73,h_73,al_c,usm_0.66_1.00_0.01,enc_auto/3DRS%20Logo.png',
+    desctiption: 'A NEW WAY TO FIND A HOME',
+    link: 'https://www.3droomspace.com/'
+  },
+
+  {
+    img: '/images/simcita.png',
+    desctiption:
+      'An experience far beyond a website, presentation tool or conference call.',
+    link: 'https://www.simcita.com/'
+  },
+  {
+    img: 'https://www.comae.com/svg/logo.svg',
+    desctiption:
+      'A Memory Focused Investigation Platform providing unique data-enrichment and query capabilities.',
+    link: 'https://www.comae.com/'
+  },
+  {
+    img: 'https://makeui.design/images/makeui-logo.svg',
+    desctiption: 'Styled Sketch UI Kits for quick prototyping',
+    link: 'https://makeui.design/'
+  },
+  {
+    img: 'https://www.pwaconnect.com/pub/media/logo/stores/1/pwaconnect.png',
+    desctiption:
+      'Multi-store and multi-warehouse management PWA built for magento 2, written in React, Magento 2.',
+    link: 'https://www.pwaconnect.com/'
+  },
+  {
+    img: 'https://uploads-ssl.webflow.com/6130b4d29bb0ab09e14ae9ee/6244bed8b4067bbaaa8dd1f4_novu-logo-gradient-formerly-notifire-dark-background-p-500.png',
+    desctiption: 'Notification Infrastructure',
+    link: 'https://www.novu.co/'
+  }
+];
+
+const contactConfig = {
+  YOUR_EMAIL: 'ericbai927@gmail.com',
+  YOUR_FONE: '(415)341-9909',
+  description:
+    'Available for contract work, freelance, and consulting. I am always open to discussing new opportunities',
+  YOUR_SERVICE_ID: 'service_id',
+  YOUR_TEMPLATE_ID: 'template_id',
+  YOUR_USER_ID: 'user_id'
+};
+
+const socialprofils = {
+  github: 'https://github.com/ericobi',
+  facebook: 'https://facebook.com',
+  linkedin: 'https://www.linkedin.com',
+  twitter: 'https://twitter.com',
+  youtube: 'https://youtube.com',
+  twitch: 'https://twitch.com'
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext
+};
